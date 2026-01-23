94c76855b820fb059973c1003c98120c076455b8
David Dzul <davidf.dzul@gmail.com>
2026-01-22 18:58:48 -0600

    fix(main): reset password view

---
+++ src/views/auth/ResetPasswordView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="login__screen-form mx-auto text-center">
+    <h3>Correo enviado</h3>
+    <p>
+      El correo con las instrucciones para reiniciar tu contraseña ha sido enviado. Si no lo has
+      recibido recuerda revisar tu carpeta de SPAM o solicitalo nuevamente.
+    </p>
+    <v-btn class="mt-3" color="primary" to="/auth/login">Regresar a Inicio</v-btn>
+  </div>
+</template>
